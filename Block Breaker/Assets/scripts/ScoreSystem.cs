d55626a9b426ab905b4be8b719061c28d1d41001
YusukZebs <simhoover9000@gmail.com>
2020-11-01 10:39:38 +0900

    Added score display feature

--- Block Breaker/Assets/scripts/ScoreSystem.cs
+++ Block Breaker/Assets/scripts/ScoreSystem.cs
@@ -1,11 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreSystem : MonoBehaviour
 {
     [SerializeField] private int currentScore = 0;
     [SerializeField] private int pointsPerBlockDestroyed = 10;
+    [SerializeField] private Text textField;
+    [SerializeField] private string currentScoreDisplayText = "SCORE: ";
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,6 @@ public class ScoreSystem : MonoBehaviour
     }
     void Update()
     {
-        
+        textField.text = currentScoreDisplayText + currentScore.ToString();
     }
 }
