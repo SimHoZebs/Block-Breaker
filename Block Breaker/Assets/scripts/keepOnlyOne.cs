e562edc1435ed7f47d567382869b353c427401cd
Sim Ho <simhoover9000@gmail.com>
2020-11-04 18:48:06 +0900

    A superbly better DontDestroyOnLoadProperties
    - Completely removed the script, replaced it with keepOnlyOne
    - Apparently child objects just follow their parent

---
+++ Block Breaker/Assets/scripts/keepOnlyOne.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class keepOnlyOne : MonoBehaviour
+{
+    void Start()
+    {
+        //fuck
+        var objCount = FindObjectsOfType<keepOnlyOne>().Length;
+
+        if (objCount > 1){
+            Destroy(gameObject);
+        }
+        else{
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+}
