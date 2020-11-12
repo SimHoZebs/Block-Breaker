4211fbc72bd35512797ad30469766fe16817bdbb
Sim Ho <simhoover9000@gmail.com>
2020-11-12 16:22:19 +0900

    Added Damage Affordance
    - Reorganized prefabs

--- Block Breaker/Assets/scripts/GlassPane.cs
+++ Block Breaker/Assets/scripts/GlassPane.cs
@@ -1,56 +1,71 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GlassPane : MonoBehaviour
 {
-    //particle system
+    #region //particle system
     [SerializeField] private GameObject particleVFX;
     private float particleLifeTime = 0f;
+    #endregion
 
-    //Break sound
+    #region //Crack VFX
+    [SerializeField] private GameObject[] crackSprites;
+    #endregion
+
+    #region //Break sound
     [SerializeField] private GameObject audioSource;
     [SerializeField] private AudioClip[] glassPaneHitSounds;
     [SerializeField] private AudioClip[] glassPaneBreakSounds;
+    #endregion
 
-    //glass pane color differentiation
-    [Tooltip("0 health is considered unbreakable.")]
-    [SerializeField] private GlassPaneTypes[] glassPaneTypes = new GlassPaneTypes[6];
-    private GlassPaneTypes glassPaneFields;
+    #region //glass pane colorHex, currHealth, totalHealth, crackSprite
     [System.Serializable] private struct GlassPaneTypes{
         public string colorHex;
-        public int health;
+        public int totalHealth;
+        public int currHealth;
+        public GameObject crackSprite;
         }
 
-    //Initialzing objects for functions in other classes
+    [Tooltip("0 health is considered unbreakable.")]
+    [SerializeField] private GlassPaneTypes[] glassPaneTypes = new GlassPaneTypes[6];
+    [SerializeField] private GlassPaneTypes thisGlassPaneFields;
+    #endregion
+
+    #region //Initialzing objects for functions in other classes
     private GlassPaneCounter glassPaneCounter;
     private ScoreSystem scoreSystem;
+    #endregion
 
     private void Start() {
         glassPaneCounter = FindObjectOfType<GlassPaneCounter>();
         scoreSystem = FindObjectOfType<ScoreSystem>();
 
         particleLifeTime = particleVFX.GetComponent<ParticleSystem>().main.duration;
-        glassPaneFields.colorHex = ColorUtility.ToHtmlStringRGB(gameObject.GetComponent<SpriteRenderer>().color);
+        thisGlassPaneFields.colorHex = ColorUtility.ToHtmlStringRGB(gameObject.GetComponent<SpriteRenderer>().color);
 
         InitializeGlassPaneFields();
     }
 
     private void InitializeGlassPaneFields(){
         foreach (GlassPaneTypes glassPaneType in glassPaneTypes){
-            if (glassPaneFields.colorHex == glassPaneType.colorHex){
-                glassPaneFields.health = glassPaneType.health;
+            if (thisGlassPaneFields.colorHex == glassPaneType.colorHex){
+                thisGlassPaneFields.totalHealth = glassPaneType.totalHealth;
+                thisGlassPaneFields.currHealth = thisGlassPaneFields.totalHealth;
+                ShowCracks();
             }
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
         AudioSystem.PlayRandomSFX(glassPaneHitSounds, audioSource);
-        if (glassPaneFields.health == 1){
+        if (thisGlassPaneFields.currHealth == 1){
             DestroyBlock();
         }
-        else if (glassPaneFields.health > 0){
-            glassPaneFields.health--;
+        else if (thisGlassPaneFields.currHealth > 0){
+            thisGlassPaneFields.currHealth--;
+            ShowCracks();
         }
     }
 
@@ -59,12 +74,23 @@ public class GlassPane : MonoBehaviour
         PlayParticleVFX();
         glassPaneCounter.reduceGlassPaneCount();
         scoreSystem.AddBlockDestroyedPoints();
+        Destroy(thisGlassPaneFields.crackSprite);
         Destroy(gameObject);
     }
 
     private void PlayParticleVFX(){
         var particles = Instantiate(particleVFX, transform.position, transform.rotation);
         Destroy(particles, particleLifeTime);
+    }
+
+    private void ShowCracks(){
+        var crackLevel = Convert.ToInt32((1- Convert.ToDouble(thisGlassPaneFields.currHealth)/Convert.ToDouble(thisGlassPaneFields.totalHealth)) * crackSprites.Length - 1);
+
+        if (crackLevel >= 0){
+            Destroy(thisGlassPaneFields.crackSprite);
+            thisGlassPaneFields.crackSprite = Instantiate(crackSprites[crackLevel], transform.position, transform.rotation, gameObject.transform);
+        }
+
     }
 
 }
